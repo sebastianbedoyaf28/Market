16fe7e240b5f7eb88e58a2c4d01fb7e3bd84d809
Carlos004266 <carlosdavidrodriguezq@gmail.com>
2025-09-24 15:15:19 -0500

    Revision conexion con supabase v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage) },
-  { path: 'home',  loadComponent: () => import('./home/home.page').then(m => m.HomePage) },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  canActivate: [AuthGuard], loadComponent: () => import('./home/home.page').then(m => m.HomePage) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // <— aquí el cambio
+  // { path: '**', redirectTo: 'login' }, // opcional
 ];
 
 @NgModule({
